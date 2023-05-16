5058c74789f8268dc8718a3a3eb1932b71eeee90
Jesus Santos <jesus@hcclsc.org>
2023-05-15 18:31:25 -0700

    a couple of changes to vars

--- script.js
+++ script.js
@@ -14,40 +14,53 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //my code
-var passLength = null;
+var passLength = prompt('How many characters long would you like your password to be?');
+var includeLowercaseLetters = confirm('Do you want to include lowercase letters?');
+var includeUppercaseLetters = confirm('Do you want to include uppercase letters?');
+var includeNumbers = confirm('Do you want to include numbers?')
+var includeSymbols = confirm('Do you want to include symbols?');
 
-function begin(passLength) {
-  passLength = prompt("How many characters long would you like your password to be?");
-  if (passLength = null) {
-    alert("Response cannot be blank. Please enter a value.");
-  } else if (passLength < 8 && passLength > 128) {
-    alert("Length must be between 8 and 128 characters.");
-  } else if (includeLowercaseLetters = confirm("Do you want to include lowercase letters?")) {
-    if (includeLowercaseLetters = true) {
-    }
 
-  } else console.log("I don't get it!!");
-}
+// function begin(passLength) {
+//   passLength = prompt("How many characters long would you like your password to be?");
+//   if (passLength = null) {
+//     alert("Response cannot be blank. Please enter a value.");
+//   } else if (passLength < 8 && passLength > 128) {
+//     alert("Length must be between 8 and 128 characters.");
+//   } else if (includeLowercaseLetters = confirm("Do you want to include lowercase letters?")) {
+//     if (includeLowercaseLetters = true) {
+//       alert("Lowercase letters included.");
+//     }
+//   } else console.log("I don't get it!!");
+// }
+
+// vars
+
+console.log(passLength);
+console.log(includeLowercaseLetters);
+console.log(includeUppercaseLetters);
+console.log(includeNumbers);
+console.log(includeSymbols);
 
 // Lowercase letters
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
 
 console.log(lowerCase[Math.floor(Math.random() * lowerCase.length)]);
 
 //Uppercase letters
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
 console.log(upperCase[Math.floor(Math.random() * upperCase.length)]);
 
 //Numbers
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = ["0123456789"];
 
 console.log(numbers[Math.floor(Math.random() * numbers.length)]);
 
 //Symbols
-const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", '-', "_", "+", "{", "[", "}", "]", "|", "=", "<", ",", ".", ">", "?"];
+const symbols = ["~`!@#$%^&*()-_+{[}]|=<,.>?"];
 
 console.log(symbols[Math.floor(Math.random() * symbols.length)]);
 
 //testing
-begin();
+// begin();
