953048296c797073aab6c313abbd3dc560fbf9eb
Jesus Santos <jesus@hcclsc.org>
2023-05-16 17:58:57 -0700

    working version

--- script.js
+++ script.js
@@ -10,17 +10,30 @@ function writePassword() {
 
 }
 
-function generatePassword(){
+function generatePassword() {
   //my code
   // variables
   var passLength = prompt('How many characters long would you like your password to be?');
-  var includeLowercaseLetters = confirm('Do you want to include lowercase letters?');
+  //input validation
+    if (passLength == null) {
+      alert("Response cannot be blank. Please enter a whole number value.");
+      return;
+    } 
+    
+    passLength=parseInt(passLength);
+
+    if (passLength < 8 || passLength > 128) {
+      alert("Length must be between 8 and 128 characters.");
+      return;
+    }
+
+    var includeLowercaseLetters = confirm('Do you want to include lowercase letters?');
   var includeUppercaseLetters = confirm('Do you want to include uppercase letters?');
   var includeNumbers = confirm('Do you want to include numbers?')
   var includeSymbols = confirm('Do you want to include symbols?');
   var selections = [];
   var gennedPW = "";
-  
+
   //constants
   // Lowercase letters
   const lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
@@ -31,35 +44,35 @@ function generatePassword(){
   //Symbols
   const symbols = "~`!@#$%^&*()-_+{[}]|=<,.>?".split("");
 
-  if(includeLowercaseLetters){
-    for (var x=0; x<lowerCase.length; x++){
+  if (includeLowercaseLetters) {
+    for (var x = 0; x < lowerCase.length; x++) {
       selections.push(lowerCase[x])
     }
   }
-  
-  if(includeUppercaseLetters){
-    for (var x=0; x<upperCase.length; x++){
+
+  if (includeUppercaseLetters) {
+    for (var x = 0; x < upperCase.length; x++) {
       selections.push(upperCase[x])
     }
   }
 
-  if(includeNumbers){
-    for (var x=0; x<numbers.length; x++){
+  if (includeNumbers) {
+    for (var x = 0; x < numbers.length; x++) {
       selections.push(numbers[x])
     }
   }
-  
-  if(includeSymbols){
-    for (var x=0; x<symbols.length; x++){
+
+  if (includeSymbols) {
+    for (var x = 0; x < symbols.length; x++) {
       selections.push(symbols[x])
     }
   }
 
-for(var x=0; x<passLength; x++){
-  gennedPW += selections[Math.floor(Math.random() * selections.length)];
-}
+  for (var x = 0; x < passLength; x++) {
+    gennedPW += selections[Math.floor(Math.random() * selections.length)];
+  }
 
-return gennedPW;
+  return gennedPW;
 
   //checks
   console.log(passLength);
@@ -81,15 +94,20 @@ generateBtn.addEventListener("click", writePassword);
 // may not need
 // function begin(passLength) {
 //   passLength = prompt("How many characters long would you like your password to be?");
-//   if (passLength = null) {
-  //     alert("Response cannot be blank. Please enter a value.");
-//   } else if (passLength < 8 && passLength > 128) {
+  
+//   if (passLength == null) {
+//       alert("Response cannot be blank. Please enter a value.");
+//   } 
+  
+//   if (passLength < 8 && passLength > 128) {
 //     alert("Length must be between 8 and 128 characters.");
-//   } else if (includeLowercaseLetters = confirm("Do you want to include lowercase letters?")) {
+//   }
+  
+  // else if (includeLowercaseLetters = confirm("Do you want to include lowercase letters?")) {
   //     if (includeLowercaseLetters = true) {
-//       alert("Lowercase letters included.");
-//     }
-//   } else console.log("I don't get it!!");
+  //     alert("Lowercase letters included.");
+  //   }
+  // } else console.log("I don't get it!!");
 // }
 
 //testing
